190ba5eb1e5c4013eb124756d35b752fe6d86ae9
Paul Merlyn <paul@paulmerlyn.com>
2019-01-15 06:46:29 -0800

    Add react-redux and redux to the project dependencies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import redux from 'redux';
+import ReactRedux from 'react-redux';
 import App from './components/App';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
